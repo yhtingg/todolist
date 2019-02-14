5f85c51d8e8f142a2529602ae0b7d1ec4fa0763d
yhtingg <huitingxd@gmail.com>
2019-02-14 17:13:50 +0800

    added validation such that deadline cannot be in the past

--- app/models/todolist.rb
+++ app/models/todolist.rb
@@ -1,4 +1,11 @@
 class Todolist < ApplicationRecord
   validates :todo, presence: true,
                    length: {minimum: 5 }
+  validate :deadline_cannot_be_in_the_past
+
+  def deadline_cannot_be_in_the_past
+    if deadline < Date.today
+      errors.add(:deadline, "can't be in the past")
+    end
+  end
 end
